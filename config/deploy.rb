b62cf71af4e9c27887c9b0df9fb8eb1904da47ea
Jérémy FRERE <frere.jeremy@gmail.com>
2024-09-22 19:34:20 +0200

    Updates config for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,10 +5,18 @@ set :application, "pecha_printer"
 set :repo_url, "git@github.com:jerefrer/pecha-printer.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/home/deploy/pecha_printer'
+
+# Default value for :linked_files is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'vendor/javascript', '.bundle', 'public/system', 'public/uploads', 'storage'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
+
+set :ssh_options, forward_agent: true
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -23,17 +31,11 @@ set :repo_url, "git@github.com:jerefrer/pecha-printer.git"
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
-
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
